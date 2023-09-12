e7c52412bd0d3463f50de9ba37ad67179b5f3490
lorem10 <momenei.alireza1382.15@gmail.com>
2023-09-12 21:55:10 +0330

    chore: change theme

--- lua/theme.lua
+++ lua/theme.lua
@@ -2,11 +2,11 @@ local set = vim.opt
 local vimscript = vim.cmd
 
 -- chack found themes
-local themes_status = pcall(require, "dracula")
+local themes_status = pcall(require, "monokai")
 
 -- auto load theme
 if themes_status then
-	require("packages.colors.dracula")
+	require("packages.colors.monokai")
 end
 
 set.syntax = "Enable"
